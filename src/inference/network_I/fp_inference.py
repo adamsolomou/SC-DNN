85e6f4adb4f0d68b6295f41241c93b2fbfa31afa
Adamos Solomou <adamossolomou@Adamoss-MacBook-Pro.local>
2018-07-02 16:47:13 +0300

    Fixing indentation

--- src/inference/network_I/fp_inference.py
+++ src/inference/network_I/fp_inference.py
@@ -14,13 +14,13 @@ n_classes = 10   # MNIST total classes (0-9 digits)
 
 # Load trained coefficients 
 w_dic = {
-	'h1':   np.genfromtxt('coeffs/W1.csv', delimiter=','),
-	'out':  np.genfromtxt('coeffs/Wout.csv', delimiter=','),
+    'h1':   np.genfromtxt('coeffs/W1.csv', delimiter=','),
+    'out':  np.genfromtxt('coeffs/Wout.csv', delimiter=','),
 }
 
 b_dic = {
-	'b1':   np.genfromtxt('coeffs/b1.csv', delimiter=','),
-	'out':  np.genfromtxt('coeffs/bout.csv', delimiter=','),
+    'b1':   np.genfromtxt('coeffs/b1.csv', delimiter=','),
+    'out':  np.genfromtxt('coeffs/bout.csv', delimiter=','),
 }
 
 # Graph input 
@@ -29,23 +29,23 @@ Y = tf.placeholder(tf.float32, [None, n_classes])
 
 # Network coefficients 
 weights = {
-	'h1': tf.placeholder(tf.float32, [n_features, n_hidden_1], name="w1"),
-	'out': tf.placeholder(tf.float32, [n_hidden_1, n_classes], name="wout")
+    'h1': tf.placeholder(tf.float32, [n_features, n_hidden_1], name="w1"),
+    'out': tf.placeholder(tf.float32, [n_hidden_1, n_classes], name="wout")
 }
 
 biases = {
-	'b1': tf.placeholder(tf.float32, [n_hidden_1], name="b1"),
-	'out': tf.placeholder(tf.float32, [n_classes], name="bout")
+    'b1': tf.placeholder(tf.float32, [n_hidden_1], name="b1"),
+    'out': tf.placeholder(tf.float32, [n_classes], name="bout")
 }
 
 # Network graph 
 def neural_net(x):
-	# Hidden layer 
-	layer_1 = tf.add( tf.matmul(x, weights['h1']), biases['b1'] ) 
-	# Output layer 
-	out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    # Hidden layer 
+    layer_1 = tf.add( tf.matmul(x, weights['h1']), biases['b1'] ) 
+    # Output layer 
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
 
-	return out_layer
+    return out_layer
 
 logits = neural_net(X)
 prediction = tf.nn.softmax(logits)
@@ -55,11 +55,11 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 with tf.Session() as sess:
 
-	# Run floating point network inference
-	print("Testing accuracy: ", 
-		sess.run(accuracy, feed_dict={X: mnist.train.images,
-									  Y: mnist.train.labels,
-									  weights['h1']:  w_dic['h1'],
-									  weights['out']: w_dic['out'],
-									  biases['b1']:   b_dic['b1'],
-									  biases['out']:  b_dic['out']}))
+    # Run floating point network inference
+    print("Testing accuracy: ", 
+        sess.run(accuracy, feed_dict={X: mnist.train.images,
+                                      Y: mnist.train.labels,
+                                      weights['h1']:  w_dic['h1'],
+                                      weights['out']: w_dic['out'],
+                                      biases['b1']:   b_dic['b1'],
+                                      biases['out']:  b_dic['out']}))
